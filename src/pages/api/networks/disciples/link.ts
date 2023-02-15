d52774e5bce985c1d653c6e0af4d4ff2651e05bf
Sam Tagalog <98756288+samtags@users.noreply.github.com>
2023-02-15 15:27:17 +0800

    chore: added admin.fishgen.org in cors

--- src/pages/api/networks/disciples/link.ts
+++ src/pages/api/networks/disciples/link.ts
@@ -24,7 +24,11 @@ export default async function handler(
   await NextCors(req, res, {
     // Options
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    origin: ["http://localhost:3001", "http://localhost:3002"],
+    origin: [
+      "http://localhost:3001",
+      "http://localhost:3002",
+      "https://admin.fishgen.org",
+    ],
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
 
